a8c16194c558ac3cdea1f16e0e2bc3c7500c644a
leley01 <wanderleysr00@gmail.com>
2022-01-28 19:45:16 -0300

    final version project

--- PigDev-master/src/Estruturas.h
+++ PigDev-master/src/Estruturas.h
@@ -4,17 +4,22 @@
 #define TAM_BLOCO 30
 #define DELTA_TIME 0.016
 
+//Declaração de variaveis importantes
 float velocidadeDaBusca = 2;
 int fonte, fonteAletaMiss, timer, qtdNiveis = 3, indiceNivelAtual = 0, elementoProcurado = 0;
-bool continuarChecando = false, missTodosNiveis = false;
+bool continuarChecando = false, missTodosNiveis = false, ocorreuHit = false;
 
+//Função responsável por resetar elementos para o seu estado inicial.
 void resetBusca(){
     indiceNivelAtual = 0;
     elementoProcurado = 0;
     continuarChecando = false;
     missTodosNiveis = false;
+    ocorreuHit = false;
 }
 
+//Classe que representa os blocos pequenos que ficam dentro dos níveis.
+//Possui variaveis importantes como valor do bloco, sua posição, o seu tamanho e sua cor.
 class Bloco{
 public:
     int numero;
@@ -22,7 +27,7 @@ public:
     int tamanho;
     PIG_Cor cor;
 
-    Bloco(int valor, int posicaoX, int posicaoY, int tamanhoBloco=TAM_BLOCO, PIG_Cor color = {0,0,255,255}){
+    Bloco(int valor, int posicaoX, int posicaoY, int tamanhoBloco=TAM_BLOCO, PIG_Cor color = {78,88,97,255}){
         numero = valor;
         iniX = posicaoX;
         iniY = posicaoY;
@@ -31,17 +36,21 @@ public:
     }
 
 public:
-
+    //Função responsável por desenhar o bloco em tela.
     void desenhaBloco(){
         DesenhaRetangulo(iniX,iniY,tamanho,tamanho,cor);
         EscreveInteiroCentralizado(numero,(iniX + tamanho/2),(iniY + tamanho/4),BRANCO,fonte);
     }
 
+    //Função responsável por alterar a cor do bloco.
     void trocaCor(PIG_Cor color){
         cor = color;
     }
 };
 
+//Classe que representa um nível( Parte grande que armazena um conjunto de blocos).
+// Possui campos importantes no seu construtor, como:
+//Sua posição, quantos elementos devem ter em uma linha, a quantidade de blocos que um nível tem, sua cor, etc.
 class Nivel{
 public:
     char * nomeNivel;
@@ -50,9 +59,12 @@ public:
     int minElementos, numElementos;
     int largura,altura;
     bool miss = false;
+    bool hit = false;
+    bool auxiliarSetPosicao = false;
+
     PIG_Cor cor;
 
-    Nivel(int posicaoX,int posicaoY,int elementosLinha,int qtdBlocos,int larg=200,int alt=100,PIG_Cor color={0,144,144,255}){
+    Nivel(int posicaoX,int posicaoY,int elementosLinha,int qtdBlocos,int larg=200,int alt=100,PIG_Cor color={158,205,191,255}){
         iniX = posicaoX;
         iniY = posicaoY;
         minElementos = elementosLinha;
@@ -62,39 +74,60 @@ public:
         cor = color;
     }
 
+    //Função responsável por desenha o nível em tela
     void desenhaNivel(){
         DesenhaRetangulo(iniX,iniY,altura,largura,cor);
-        //EscreveInteiroCentralizado(numero,(iniX + tamanho/2),(iniY + tamanho/4),BRANCO,fonte);
     }
 
+    //Função responsável por verificar que blocos não sejam iniciados com valores iguais.
+    bool validaValor(int valor){
+        for(int i=0;i<elementos.size();i++){
+            if(elementos[i].numero == valor){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //Função responsável por criar os blocos, gerando valores aleatorios e setando as duas posições.
     void criarBlocos(int Qtd){
         for(int i = 0; i<Qtd; i++){
             int espacos = (largura - (TAM_BLOCO*minElementos))/(minElementos+1);
+            int auxValor = 0;
 
-            ////////////////////////ALTERAR O VALOR DO BLOCO
-            Bloco auxBloco = Bloco(rand()%999, iniX + (espacos*((i%minElementos)+1)) + (TAM_BLOCO*(i%minElementos)),(iniY+altura) - (espacos*((i/minElementos)+1)) - (TAM_BLOCO*((i/minElementos)+1)),TAM_BLOCO);
+            do{
+                auxValor = (rand()%998)+1;
+            }while(validaValor(auxValor) == false);
+
+            Bloco auxBloco = Bloco(auxValor, iniX + (espacos*((i%minElementos)+1)) + (TAM_BLOCO*(i%minElementos)),(iniY+altura) - (espacos*((i/minElementos)+1)) - (TAM_BLOCO*((i/minElementos)+1)),TAM_BLOCO);
             elementos.push_back(auxBloco);
         }
     }
 
+    //Função responsável por resetar variaveis dos blocos e niveis importantes.
     void resetBlocos(){
         for(int i=0;i<elementos.size();i++){
-            elementos[i].trocaCor({0,0,255,255});
+            elementos[i].trocaCor({78,88,97,255});
         }
         ind = 0;
         tempo = 0;
         ReiniciaTimer(timer,true);
         miss = false;
+        hit = false;
+        auxiliarSetPosicao = false;
     }
 
-    void escreveAlerta(){
-        EscreverEsquerda("Cache Miss!",iniX+largura+10,iniY+(altura/2)-10,BRANCO,fonteAletaMiss);
+    //Função responsável por escrever um alerta ao lado do nível.
+    void escreveAlerta(char * alerta){
+        EscreverEsquerda(alerta,iniX+largura+10,iniY+(altura/2)-10,{78,88,97,255},fonteAletaMiss);
     }
 
+    //Função responsável por escrever uma string no centro do nível.
     void escreveNomeNivel(){
-        EscreverCentralizada(nomeNivel,iniX+(largura/2),iniY+(altura/2),BRANCO,fonteAletaMiss);
+        EscreverCentralizada(nomeNivel,iniX+(largura/2),iniY+(altura/2),{78,88,97,255},fonteAletaMiss);
     }
 
+    //Função responsável por desenhar os elementos(blocos) do nível.
     void desenharElementos(){
         for(int i=0;i<elementos.size();i++){
             elementos[i].desenhaBloco();
@@ -102,23 +135,27 @@ public:
     }
 
     float tempo,ind =0;
+
+    //Função responsável por geranciar a busca. Ela que verifica se o valor foi procurado, se a busca deve ir para outro nivel,
+    //se ocorreu cache hit ou miss. Gerencia a busca em geral.
     void procuraBloco(){
-        if(TempoDecorrido(timer)> DELTA_TIME){
+        if(TempoDecorrido(timer)> DELTA_TIME && ocorreuHit == false){
             if(tempo > velocidadeDaBusca){
                 if(elementos[ind].numero == elementoProcurado){
-                    elementos[ind].cor ={0,255,0,255};
+                    elementos[ind].cor ={68,170,72,255};
+                    ocorreuHit = true;
+                    hit = true;
                     continuarChecando =  false;
                 }else{
-                    elementos[ind].cor ={255,0,0,255};
+                    elementos[ind].cor ={239,73,65,255};
                 }
                 ReiniciaTimer(timer,true);
                 tempo=0;
                 ind++;
-                if(ind == elementos.size()){
+                if(ind == elementos.size() && ocorreuHit == false){
                     ind=0;
                     indiceNivelAtual++;
                     miss = true;
-                    //if(indiceNivelAtual%qtdNiveis == 0){
                     if(indiceNivelAtual == qtdNiveis){
                         continuarChecando = false;
                         missTodosNiveis = true;
@@ -131,7 +168,7 @@ public:
         }else{
             DespausaTimer(timer);
             tempo = 0;
-            elementos[ind].cor ={255,255,0,255};
+            elementos[ind].cor ={246,167,33,255};
         }
     }
 
--- PigDev-master/src/main.cpp
+++ PigDev-master/src/main.cpp
@@ -4,6 +4,7 @@
 #define WIDTH 1000
 #define HEIGHT 600
 #define DELTA_TIME 0.016
+#define TEMPO_ESPERA 5.0
 
 PIG_Evento evento;          //evento ser tratadoi a cada pssada do loop principal
 PIG_Teclado meuTeclado;     //variável como mapeamento do teclado
@@ -16,15 +17,20 @@ int main( int argc, char* args[] ){
     fonte = CriaFonteNormal("../fontes/arial.ttf", 12,BRANCO);
     fonteAletaMiss = CriaFonteNormal("../fontes/arial.ttf", 16,BRANCO);
     timer = CriaTimer(true);
+    SetCorFundoJanela({225,230,211,255});
 
 
     //associando o teclado (basta uma única vez) com a variável meuTeclado
+
+    //Declaração de variaveis importantes
     srand(time(NULL));
     bool terminou = false;
     int temporizador = CriaTimer(true);
     meuTeclado = GetTeclado();
     char elementoBuscado[20] =  "";
     int tamanhoElementoBuscado = 0;
+
+    //Criação dos niveis e seus blocos que aparecerão na tela.
     Nivel nivel1 = Nivel(250,400,4,4,200,62);
     nivel1.criarBlocos(4);
 
@@ -36,23 +42,18 @@ int main( int argc, char* args[] ){
 
     Nivel memPrincipal = Nivel(780,100,0,0,200,400);
 
-    //Niveis troca ocorrencia miss
+    //Nivel usado na atualização dos níveis na ocorrencia de um cache miss
     Nivel blocoDeTrocaMiss = Nivel(780,280,1,1,40,40);
     blocoDeTrocaMiss.criarBlocos(1);
-    /*Nivel blocoDeTrocaMiss1 = Nivel(780,280,1,1,40,40);
-    blocoDeTrocaMiss1.criarBlocos(1);
 
-    Nivel blocoDeTrocaMiss2 = Nivel(780,280,1,1,40,40);
-    blocoDeTrocaMiss2.criarBlocos(1);
+    //Nivel usado na atualização dos níveis na ocorrencia de um cache hit
+    Nivel blocoDeTrocaHit = Nivel(780,280,1,1,40,40);
+    blocoDeTrocaHit.criarBlocos(1);
 
-    Nivel blocoDeTrocaMiss3 = Nivel(780,280,1,1,40,40);
-    blocoDeTrocaMiss3.criarBlocos(1);*/
-
-    /*vector<Nivel *> NiveisMiss = {&blocoDeTrocaMiss1,&blocoDeTrocaMiss2,&blocoDeTrocaMiss3};*/
-    // --------//
-
+    //Array com os niveis desenhados em tela
     vector<Nivel *> Niveis = {&nivel1,&nivel2,&nivel3};
     int indiceNivelAtt = Niveis.size()-1;
+
     //loop principal do jogo
     while(JogoRodando()){
 
@@ -64,6 +65,7 @@ int main( int argc, char* args[] ){
         //será feita aprepação do frame que será exibido na tela
         IniciaDesenho();
 
+        //Inicia o desenho dos níveis e blocos na tela
         nivel1.desenhaNivel();
         nivel1.desenharElementos();
 
@@ -77,8 +79,9 @@ int main( int argc, char* args[] ){
         memPrincipal.nomeNivel = "Memoria Principal";
         memPrincipal.escreveNomeNivel();
 
-        //Operação teclado escrever numero buscado
-        if( evento.tipoEvento == PIG_EVENTO_TECLADO && continuarChecando == false){
+        //Responsável pelos eventos do teclado.Pega os valores digitados no teclado numero(na parte de cima do teclado).
+        //Backspace apaga um valor digitado.
+        if( evento.tipoEvento == PIG_EVENTO_TECLADO && continuarChecando == false && terminou == false){
             if(evento.teclado.acao == PIG_TECLA_LIBERADA){
 
                 if(evento.teclado.tecla == PIG_TECLA_0){
@@ -120,13 +123,14 @@ int main( int argc, char* args[] ){
             }
         }
 
-        EscreverEsquerda("Valor Buscado:",15,500);
-        EscreveInteiroEsquerda(atoi(elementoBuscado),280,500);
+        //Escreve na tela o valor que esta sendo digitado.
+        EscreverEsquerda("Valor Buscado:",15,500,{78,88,97,255});
+        EscreveInteiroEsquerda(atoi(elementoBuscado),280,500,{78,88,97,255});
 
-        // Reset busca
+        // Responsável por resetar todas as variaveis necessarias para o seu estado inicial
         if( meuTeclado[PIG_TECLA_ESC]){
             resetBusca();
-            memset(elementoBuscado, ' ', 20);
+            memset(elementoBuscado, 0, 20);
             tamanhoElementoBuscado = 0;
             nivel1.resetBlocos();
             nivel2.resetBlocos();
@@ -140,10 +144,13 @@ int main( int argc, char* args[] ){
             blocoDeTrocaMiss.elementos[0].iniY = 285;
         }
 
+        //Responsável por iniciar a busca do elemento
         if(meuTeclado[PIG_TECLA_ENTER]){
             continuarChecando = true;
             elementoProcurado = atoi(elementoBuscado);
         }
+
+        //Responsável por controlar a velocidade da buscar e verificar se a busca deve continuar sendo feita ou não
         if(continuarChecando == true){
             Niveis[indiceNivelAtual%qtdNiveis]->procuraBloco();
 
@@ -151,21 +158,27 @@ int main( int argc, char* args[] ){
             if( evento.tipoEvento == PIG_EVENTO_TECLADO){
                 if(evento.teclado.acao == PIG_TECLA_LIBERADA){
                     if(evento.teclado.tecla == PIG_TECLA_ESQUERDA){
-                        velocidadeDaBusca*=2;
+                        velocidadeDaBusca = velocidadeDaBusca*2 > 5 ? 5 : velocidadeDaBusca*2;
                     }else if(evento.teclado.tecla == PIG_TECLA_DIREITA){
-                        velocidadeDaBusca/=2;
+                        velocidadeDaBusca = velocidadeDaBusca/2 < 0.25 ? 0.25 : velocidadeDaBusca/2;
                     }
                 }
             }
         }
 
+        //Percorre todos os niveis verificando pontos importantes.
         for(int i=0;i<qtdNiveis;i++){
             if(Niveis[i]->miss){
-                Niveis[i]->escreveAlerta();
+                Niveis[i]->escreveAlerta("Cache Miss!");
+            }else if(Niveis[i]->hit){
+                Niveis[i]->escreveAlerta("Cache Hit!");
             }
 
-            if(missTodosNiveis && !terminou){
+            //Caso o bloco de dados não tenha sido encontrado em nenhum nível, um bloco sai da memoria atualizando todos os
+            //níveis.
+            if( missTodosNiveis && !terminou){
                 Niveis[indiceNivelAtt]->miss = false;
+
                 if((Niveis[indiceNivelAtt]->iniY + Niveis[indiceNivelAtt]->altura/2)-20 < blocoDeTrocaMiss.iniY){
                     blocoDeTrocaMiss.iniY--;
                     blocoDeTrocaMiss.elementos[0].iniY--;
@@ -178,7 +191,7 @@ int main( int argc, char* args[] ){
                 }else{
                     int indiceAux = rand()%Niveis[indiceNivelAtt]->numElementos;
                     Niveis[indiceNivelAtt]->elementos[indiceAux].numero = elementoProcurado;
-                    Niveis[indiceNivelAtt]->elementos[indiceAux].cor = LARANJA;
+                    Niveis[indiceNivelAtt]->elementos[indiceAux].cor = {104,178,248,255};
                     indiceNivelAtt--;
                     if(indiceNivelAtt < 0){
                         //reset a aplicacao
@@ -190,11 +203,52 @@ int main( int argc, char* args[] ){
                 blocoDeTrocaMiss.desenhaNivel();
                 blocoDeTrocaMiss.elementos[0].numero = atoi(elementoBuscado);
                 blocoDeTrocaMiss.desenharElementos();
+
+            //Caso tenha ocorrido um cache hit, um bloco saí desse nível atualizando todos os superiores
+            }else if(ocorreuHit && !terminou){
+                if(indiceNivelAtual>0){
+                    if(Niveis[indiceNivelAtual]->auxiliarSetPosicao == false){
+                        blocoDeTrocaHit.iniX = (Niveis[indiceNivelAtual]->iniX + Niveis[indiceNivelAtual]->largura)+8;
+                        blocoDeTrocaHit.iniY = (Niveis[indiceNivelAtual]->iniY + Niveis[indiceNivelAtual]->altura/2)-20;
+                        blocoDeTrocaHit.elementos[0].iniX = blocoDeTrocaHit.iniX + 5;
+                        blocoDeTrocaHit.elementos[0].iniY = blocoDeTrocaHit.iniY + 5;
+                        Niveis[indiceNivelAtual]->auxiliarSetPosicao = true;
+                    }
+                    if((Niveis[indiceNivelAtual-1]->iniY + Niveis[indiceNivelAtual-1]->altura/2)-20 > blocoDeTrocaHit.iniY){
+                        blocoDeTrocaHit.iniY++;
+                        blocoDeTrocaHit.elementos[0].iniY++;
+                    }else if((Niveis[indiceNivelAtual-1]->iniX + Niveis[indiceNivelAtual-1]->largura)+8 < blocoDeTrocaHit.iniX){
+                        blocoDeTrocaHit.iniX--;
+                        blocoDeTrocaHit.elementos[0].iniX--;
+                    }else{
+
+                        indiceNivelAtual--;
+                        int indiceAux = rand()%Niveis[indiceNivelAtual]->numElementos;
+                        Niveis[indiceNivelAtual]->miss = false;
+                        Niveis[indiceNivelAtual]->elementos[indiceAux].numero = elementoProcurado;
+                        Niveis[indiceNivelAtual]->elementos[indiceAux].cor = {104,178,248,255};
+                        if(indiceNivelAtual <= 0){
+                            //reset a aplicacao
+                            terminou = true;
+                            DespausaTimer(temporizador);
+                        }
+                    }
+
+                    blocoDeTrocaHit.desenhaNivel();
+                    blocoDeTrocaHit.elementos[0].numero = atoi(elementoBuscado);
+                    blocoDeTrocaHit.desenharElementos();
+                }else{
+                    terminou = true;
+                    DespausaTimer(temporizador);
+                }
             }
+
+            //Caso a execução da busca e atualização tenha se encerrado, espera um tempo configurável e depois reseta toda
+            //a aplicação, voltando para o seu estado inicial.
             if(terminou){
-                if(TempoDecorrido(temporizador) > 15.0){
+                if(TempoDecorrido(temporizador) > TEMPO_ESPERA){
                     resetBusca();
-                    memset(elementoBuscado, ' ', 20);
+                    memset(elementoBuscado, 0, 20);
                     tamanhoElementoBuscado = 0;
                     nivel1.resetBlocos();
                     nivel2.resetBlocos();
@@ -206,9 +260,12 @@ int main( int argc, char* args[] ){
                     indiceNivelAtt = Niveis.size()-1;
                     ReiniciaTimer(temporizador,true);
                     terminou = false;
+                    //----//
+                    elementoProcurado = 0;
                 }
             }
         }
+
         //todas as chamadas de desenho devem ser feitas aqui na ordem desejada
         //o frame totalmente pronto será mostrado na tela
         EncerraDesenho();
